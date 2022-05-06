ac7911e14814b6cb30511e9aad46ab6ca95d0cfd
Kolya <87275865+Kolya142@users.noreply.github.com>
2022-05-06 23:24:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,17 +4,18 @@
 
 import os #import "os"
 import time #import "time"
-import filecmp #import "filecmp"
-
-app = "notepad.exe" #set  programm "file"
-timeout = 3 #set  timeout
-f1 = "start.log" #set  start "log" file
-f2 = "list.log" #set  programm "log" file
-os.system("start " + app)
+app = '"Among Us.exe"' #set  programm "file"
+timeout = 2 #set  timeout
+os.system("listdlls.exe " + app + " >start.log") #save start "log" file
+f1 = open("start.log") #set  start "log" file
+f1 = f1.read()
 while app == app:
     os.system("listdlls.exe " + app + " >list.log") #save programm "log" file
+    os.system("cls")
+    f2 = open("list.log") #set  programm "log" file
+    f2 = f2.read()
+    print(f2)
     time.sleep(timeout)     #timeout
-    result = filecmp.cmp(f1,f2)   #save result
-    if result == 0: #if files is different
+    if not f1 == f2: #if files is different
         os.system("taskkill /f /im " + app) #kill process
         break
